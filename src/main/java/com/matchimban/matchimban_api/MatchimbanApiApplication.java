a3a24d3491cd911600a669c917bf63b35ce78656
redjin <hongjin914@gmail.com>
2026-01-19 15:04:12 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/main/java/com/matchimban/matchimban_api/MatchimbanApiApplication.java
@@ -0,0 +1,13 @@
+package com.matchimban.matchimban_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchimbanApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchimbanApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matchimban/matchimban_api/MatchimbanApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matchimban.matchimban_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchimbanApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
