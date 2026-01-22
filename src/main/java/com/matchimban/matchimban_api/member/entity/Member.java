46cadbdf4b0dcd93998732f9780f1221b5dfcda2
Kimdonghyuk0 <kdh367036@naver.com>
2026-01-22 16:14:26 +0900

    chore: user->member(#2)

--- src/main/java/com/matchimban/matchimban_api/member/entity/Member.java
+++ src/main/java/com/matchimban/matchimban_api/member/entity/Member.java
@@ -16,7 +16,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "users")
+@Table(name = "members")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
